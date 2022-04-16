aff6c356259fe1167fb16dd54dcdb566c9e60997
Greg-rez <71189485+Greg-rez@users.noreply.github.com>
2022-04-16 12:12:22 -0700

    Add files via upload

---
+++ Implementation_projec/Client/myClient.py
@@ -0,0 +1,197 @@
+# Plans for client
+# User needs to be able to upload, download, and delete files from the server
+# Add list function that list files on server.
+# when sending always encode() and when recv always decode().
+#
+import socket
+import os
+import tqdm
+
+# Variables for socket to use
+BUFFER_SIZE = 1024  # Standard size
+SEPARATOR = "<SEPARATOR>"  # Used to make uploading files easier
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+# Handles connection to the server.
+def connect(TCP_connect = '127.0.0.1', TCP_port = 7274):
+    print("Attempting to establish connection with server")
+    try:
+        sock.connect((TCP_connect, TCP_port))
+    except:
+        print("Connection failed, either connection failed or wrong command (Im so sorry)")
+    print("Connection established")
+
+
+"""
+# Handles disconnection from server. Will probably add a way to close client as well since this shuts down the server
+def disconnect():
+    sock.send("disconnect".encode())
+    # Wait for server go-ahead
+    sock.recv(BUFFER_SIZE)
+    sock.close()
+    print("Server connection ended")
+    return
+"""
+
+# functions responsible for file management on server. (need to find out how server selects files)
+
+# Test function to see if server receives commands properly
+# Writes messages to be printed out by the server (REMOVE LATER)
+def write():
+    try:
+        sock.send("write".encode())  # Sends command to server
+        msg = input("enter text:")  # User inputs their message here
+    except:
+        print("Failed to send message. Check server status")
+        return  # ends function upon failure
+    try:
+        sock.send(msg.encode())  # Sends user message to server
+        return  # ends function
+    except:
+        print("\nStep 2 failed.")
+        return  # ends function upon failure
+
+
+# Download files from the server to the client
+def download():
+    try:
+        # sends download command to server
+        sock.send("download".encode())
+    except:
+        print("failed to send command check server")
+        return
+    # receives count from server.
+    val = sock.recv(BUFFER_SIZE).decode('ascii')
+    count = int(val)
+    # Tells server its ready to receive file list
+    sock.send("1".encode())
+    # list files that can be downloaded on the client
+    while count > 0:
+        print(sock.recv(BUFFER_SIZE).decode('ascii'))
+        count -= 1
+    # Sends file to server
+    try:
+        filename = input("Enter the name of the file to download: ")
+        sock.send(filename.encode())
+    except:
+        print("failed to verify file name from server")
+        return
+    # Receives file name and size
+    try:
+        user_data = sock.recv(BUFFER_SIZE).decode()
+    except:
+        print("Error receiving data from client")
+        return
+    # removes file SEPARATOR from user_data
+    filename, filesize = user_data.split(SEPARATOR)
+    # remove file path if there is one
+    filename = os.path.basename(filename)
+    # convert to integer
+    filesize = int(filesize)
+    # start receiving the file from the server
+    progress = tqdm.tqdm(range(filesize), f"Receiving{filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "wb") as f:
+        while True:
+            print("receiving data...")
+            # read 1024 bytes from the server (receive)
+            bytes_read = sock.recv(BUFFER_SIZE)
+            if not bytes_read:
+                print("File download {} complete".format(filename))
+                break
+            # write to the file the bytes we just received
+            f.write(bytes_read)
+            progress.update(len(bytes_read))
+    sock.close()
+
+
+# command that handles file deletion
+def delete():
+    try:
+        # sends download command to server
+        sock.send("delete".encode())
+    except:
+        print("failed to send command check server")
+        return
+    # receives count from server.
+    val = sock.recv(BUFFER_SIZE).decode('ascii')
+    count = int(val)
+    # Tells server its ready to receive file list
+    sock.send("1".encode())
+    # As long as the number of files is higher than 0 List them on client.
+    while count > 0:
+        print(sock.recv(BUFFER_SIZE).decode('ascii'))
+        count -= 1
+    # Sends the file requested to be deleted
+    filename = input("Enter the name of the file to delete ")
+    sock.send(filename.encode())
+    print(sock.recv(BUFFER_SIZE).decode('ascii'))
+    # Verification of file deleted
+    print("File has been deleted")
+    return
+
+
+#  Uploads file to the location of the server
+def upload():
+    print("choose a file to upload")
+    for name in os.listdir():
+        if not name.endswith(".py"):
+            print(name)
+    try:
+        sock.send("upload".encode())
+        sock.recv(BUFFER_SIZE)
+        filename = input("Enter the name of the file to upload: ")
+    except:
+        print("failed to send command. Check connection to server\n")
+        return
+    # gets size of file
+    filesize = os.path.getsize(filename)
+    try:
+        # Sends filename and Size to the server.
+        sock.send(f"{filename}{SEPARATOR}{filesize}".encode())
+    except:
+        print("File failed to send. Check connection")
+    progress = tqdm.tqdm(range(filesize), f"Sending{filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "rb") as f:
+        while True:
+            # read the bytes from the file
+            bytes_read = f.read(BUFFER_SIZE)
+            if not bytes_read:
+                print("File transmission complete")
+                break
+            sock.sendall(bytes_read)
+            # updates progress bar
+            progress.update(len(bytes_read))
+    sock.close()
+
+
+while True:
+    sel = input("Would you like to use default address y/n?:  ")
+    if sel == 'y':
+        connect()
+    else:
+        TCP_connect = input("Enter ip address:  ")
+        TCP_port = int(input("Enter a portnumber"))
+        connect(TCP_connect, TCP_port)
+
+
+    print("Currently there are 4 commands that are Usable\n"
+          + "     write: prints text to the screen of the server\n"
+          + "    upload: Uploads a file from the folder the clients application is located\n"
+          + "  download: Downloads a file from the folder the server application is located\n"
+          + "    delete: Deletes a file from the server folder.")
+    prompt = input("Enter command:")
+    # Nested if else that reads prompt for commands
+    if prompt == "write":
+        write()
+    elif prompt == "upload":
+        upload()
+        break
+    elif prompt == "download":
+        download()
+        break
+    elif prompt == "delete":
+        delete()
+        break
+    else:
+        print("command not recognised:{}".format(prompt))
---
+++ Implementation_projec/Server/serCommands.py
@@ -0,0 +1,125 @@
+
+import os
+import tqdm
+
+
+# Figure a way to implement this like a switch statement because the coding on this gets redundant very fast
+# And it's super annoying. Also, it may make it easier to implement ldap and other things with this doc just
+# being used for commands
+
+SEPARATOR = "<SEPARATOR>"
+
+# Prints message received from clients. (REMOVE LATER)
+def write(conn):
+    # Turns the received message to ascii string
+    msg = conn.recv(1024).decode("utf-8")
+    print(msg)
+    conn.close()
+    return
+
+
+# Receives file from client and adds file to folder application is stored in.
+def upload(conn):
+    #  Tells the client it's ready to receive files
+    conn.send("1".encode())
+    #  Data user has uploaded form the client socket
+    try:
+        user_data = conn.recv(1024).decode()
+    except:
+        print("Error receiving data from client")
+        return
+    filename, filesize = user_data.split(SEPARATOR)
+    # remove file path if there is one
+    filename = os.path.basename(filename)
+    # convert to integer
+    filesize = int(filesize)
+    # start receiving the file from the socket
+    progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    # opens file and begins to write the file to the server folder
+    with open('files/' + filename, "wb") as f:
+        while True:
+            # read 1024 bytes from the socket (receive)
+            bytes_read = conn.recv(1024)
+            if not bytes_read:
+                print("File upload {} complete".format(filename))
+                break
+            # write to the file the bytes we just received
+            f.write(bytes_read)
+            # update the progress bar
+            progress.update(len(bytes_read))
+
+
+# Sends a file to the client from the server
+def download(conn):
+    # counts the number of files in the directory
+    count = 0
+    # counts file in directory. Does not count the server file.
+    for name in os.listdir('files'):
+        if not name.endswith(".py"):
+            count += 1
+    val = str(count)
+    # sends number of files in server directory to client.
+    conn.send(val.encode())
+    # Gets ok from client to start sending file list
+    conn.recv(1024)
+    # sends file name to client in a list.
+    for name in os.listdir('files'):
+        if not name.endswith(".py"):
+            conn.send(name.encode())
+    #  Data user has uploaded form the client socket.
+    try:
+        filename = conn.recv(1024).decode('ascii')
+    except:
+        print("failed to verify file name")
+    # gets size of file
+    filesize = os.path.getsize('files/' + filename)
+    try:
+        # Sends filename and Size to the client
+        conn.send(f"{filename}{SEPARATOR}{filesize}".encode())
+    except:
+        print("File failed to send. Check connection.")
+        return
+    progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    # opens file in server and begins to write it to file.
+    with open('files/' + filename, "rb") as f:
+        while True:
+            # read the bytes from the file
+            bytes_read = f.read(1024)
+            conn.send(bytes_read)
+            if not bytes_read:
+                print("File transmission complete")
+                break
+            # updates progress bar
+            progress.update(len(bytes_read))
+    conn.close()
+    #sock.close()
+
+def delete(conn):
+    # counts the number of files in the directory
+    count = 0
+    for name in os.listdir('files'):
+        if not name.endswith(".py"):
+            count += 1
+    val = str(count)
+    # sends number of files in server directory to client.
+    conn.send(val.encode())
+    # Gets ok from client to start sending file list
+    conn.recv(1024)
+    # sends file name to client in a list.
+    for name in os.listdir('files'):
+        if not name.endswith(".py"):
+            conn.send(name.encode())
+    # Receives file requested for deletion from client.
+    try:
+        filename = conn.recv(1024).decode('ascii')
+    except:
+        print("failed to receive name of file")
+        return
+    # Checks if the file path is real or not.
+    if os.path.isfile('files/' + filename):
+        os.remove('files/' + filename) # removes file
+        print("File deleted")
+    else:
+        print("Error: check name of file")
+    return
+
